8f32284ae27437c3aabf195ab379583d71e43b10
fbatlos <bdlsfrancisco@iesrafaelalberti.es>
2024-04-24 21:33:17 +0200

    Hacer login como lista estudiante

---
+++ src/main/kotlin/Scroll/Scrollbars.kt
@@ -0,0 +1,87 @@
+package Scroll
+
+import androidx.compose.foundation.HorizontalScrollbar
+import androidx.compose.foundation.VerticalScrollbar
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.rememberScrollbarAdapter
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.WindowState
+import androidx.compose.ui.window.singleWindowApplication
+
+fun main() = singleWindowApplication(
+    title = "Scrollbars",
+    state = WindowState(width = 250.dp, height = 400.dp)
+) {
+    Box(
+        modifier = Modifier.fillMaxSize()
+            .background(color = Color(180, 180, 180))
+            .padding(10.dp)
+    ) {
+        //Cuanto de Scroll tiene que ponr d eforma vertical y horizontal
+        val stateVertical = rememberScrollState(0)
+        val stateHorizontal = rememberScrollState(0)
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                //Inicia el Scroll vertical
+                .verticalScroll(stateVertical)
+                .padding(end = 12.dp, bottom = 12.dp)
+                //Inicia el Scroll horizontal
+                .horizontalScroll(stateHorizontal)
+        ) {
+            Column {
+                //crea estos items
+                for (item in 0..30) {
+                    TextBox("Item #$item")
+                    if (item < 30) {
+                        Spacer(modifier = Modifier.height(5.dp))
+                    }
+                }
+            }
+        }
+        //Scroll vertical centrado , adaptado y mmaximizado
+        VerticalScrollbar(
+            modifier = Modifier.align(Alignment.CenterEnd)
+                .fillMaxHeight(),
+            adapter = rememberScrollbarAdapter(stateVertical)
+        )
+        //Scroll Horizontal centrado , adaptado y mmaximizado
+        HorizontalScrollbar(
+            modifier = Modifier.align(Alignment.BottomStart)
+                .fillMaxWidth()
+                .padding(end = 12.dp),
+            adapter = rememberScrollbarAdapter(stateHorizontal)
+        )
+    }
+}
+
+@Composable
+fun TextBox(text: String = "Item") {
+    Box(
+        modifier = Modifier.height(32.dp)
+            .width(400.dp)
+            .background(color = Color(200, 0, 0, 20))
+            .padding(start = 10.dp),
+        contentAlignment = Alignment.CenterStart
+    ) {
+        Text(text = text)
+    }
+}
--- src/main/kotlin/loginDiego.kt
+++ src/main/kotlin/loginDiego.kt
@@ -1,15 +1,12 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.ContextMenuDataProvider
-import androidx.compose.foundation.ContextMenuItem
+
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material.*
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.singleWindowApplication
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -17,13 +14,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.DpSize
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
-import java.awt.SystemColor.text
 
 fun main() = application {
     val windowState = rememberWindowState(size = DpSize(1200.dp,800.dp))
@@ -38,14 +32,58 @@ fun main() = application {
     }
 }
 
+@Composable
+fun Usuario(Usuario:String, onUsuarioChange: (String) -> Unit){
+    Spacer(Modifier.height(100.dp))
+    OutlinedTextField(
+        value = Usuario,
+        onValueChange = onUsuarioChange,
+        label = { Text(text = "Usuario") },
+        modifier = Modifier
+            .size(200.dp, 60.dp)
+    )
+    Spacer(Modifier.height(25.dp))
+}
 
+@Composable
+fun Contrasenia(Contrasenia: String, onContraseniaChange: (String) -> Unit){
+    var active by remember { mutableStateOf(false) }
+
+    OutlinedTextField(
+        value = Contrasenia,
+        onValueChange = onContraseniaChange,
+        label = { Text(text = "Contraseña") },
+        modifier = Modifier
+            .size(170.dp, 60.dp),
+
+        visualTransformation = if (!active) PasswordVisualTransformation() else VisualTransformation.None,
+    )
+    Button(
+        modifier = Modifier
+            .size(30.dp, 30.dp),
+        onClick = {
+            if (!active) active = true else active = false
+        }) { }
+}
+
+@Composable
+fun BottonLogin(Usuario: String,Contrasenia: String,onBotonChange:(String) -> Unit){
+    Spacer(Modifier.height(25.dp))
+    Button(
+        onClick = {
+            onBotonChange(Usuario)
+            onBotonChange(Contrasenia)
+        }) {
+        Text(" Login ", fontFamily = FontFamily.SansSerif)
+    }
+}
 
 @Composable
 @Preview
 fun login() {
-    val Usuario = remember { mutableStateOf("") }
+    var Usuario by remember { mutableStateOf("") }
 
-    val Contrasenia = remember { mutableStateOf("") }
+    var Contrasenia by remember { mutableStateOf("") }
 
     Box(
 
@@ -55,65 +93,35 @@ fun login() {
 
     ) {
         Surface(
-
             border = BorderStroke(2.dp, color = Color.White),
             modifier = Modifier.size(300.dp, 400.dp).align(Alignment.Center)
 
         ) {
-            Column() {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
 
-                Spacer(Modifier.height(100.dp))
-
-                OutlinedTextField(
-                    value = Usuario.value,
-                    onValueChange = { Usuario.value = it },
-                    label = { Text(text = "Usuario") },
-                    modifier = Modifier
-                        .align(Alignment.CenterHorizontally)
-                        .size(200.dp, 60.dp)
-                )
-
-                Spacer(Modifier.height(25.dp))
+                Usuario(Usuario){
+                    Usuario=it
+                }
 
                 Row(
                     modifier = Modifier
                         .align(Alignment.CenterHorizontally)
                 ) {
 
-                    var active by remember { mutableStateOf(false) }
-
-                    OutlinedTextField(
-                        value = Contrasenia.value,
-                        onValueChange = { Contrasenia.value = it },
-                        label = { Text(text = "Contraseña") },
-                        modifier = Modifier
-                            .align(Alignment.CenterVertically)
-                            .size(170.dp, 60.dp),
-
-                        visualTransformation = if (!active) PasswordVisualTransformation() else VisualTransformation.None,
-
+                    Contrasenia(Contrasenia){
+                        Contrasenia = it
+                    }
                        // trailingIcon = {}
-                    )
                     //cambiar button por un trailingIcon.
-                    Button(
-                        modifier = Modifier
-                            .size(30.dp, 30.dp)
-                            .align(Alignment.CenterVertically),
-                        onClick = {
-                            if (!active) active = true else active = false
-                        }) { }
                 }
 
-                Spacer(Modifier.height(25.dp))
-
-
-                Button(modifier = Modifier.align(Alignment.CenterHorizontally),
-                        onClick = {
-                            Usuario.value=""
-                            Contrasenia.value=""
-                        }) {
-                    Text(" Login ", fontFamily = FontFamily.SansSerif)
+                BottonLogin(Usuario,Contrasenia){
+                    Usuario = ""
+                    Contrasenia=""
                 }
+
             }
         }
     }
